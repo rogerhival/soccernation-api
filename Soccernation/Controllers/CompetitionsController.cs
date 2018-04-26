fd7699e4bbd4d0126cbdb93a5e0595ebff06c220
Fernando Marostega <fmarostega@gmail.com>
2018-04-27 00:45:54 +1000

    adicionado metodo temporario pra testar ladder

--- Soccernation/Controllers/CompetitionsController.cs
+++ Soccernation/Controllers/CompetitionsController.cs
@@ -99,5 +99,85 @@ namespace Soccernation.Controllers
 
             return Ok(competition.Fixtures);
         }
+
+
+        [HttpGet]
+        [Route("{competitionId}/ladder")]
+        public IActionResult GetLadder(Guid competitionId)
+        {
+            //*******************************
+            // Temp method just for test
+            //*******************************
+            var competition = GetById(competitionId);
+            if (competition == null)
+                return BadRequest();
+
+            var resultRows = new List<ResultRow>();
+            var teams = competition.Teams;
+
+            foreach (var item in teams)
+            {
+                var resultRow = new ResultRow();
+                resultRow.Team = item;
+                FixtureCalculator(competition, item, resultRow);
+                resultRows.Add(resultRow);
+            }
+
+            return Ok(resultRows.OrderByDescending(o => o.Points).ThenByDescending(o => o.Wins).ThenByDescending(o => o.Draws).ThenByDescending(o => o.Loses).ThenBy(o => o.Team.Name));
+        }
+
+        void FixtureCalculator(Competition competition, Team team, ResultRow resultRow)
+        {
+            var fixtures = competition.Fixtures.Where(f => f.Status == FixtureStatus.Finished && (f.TeamHome == team || f.TeamVisitor == team)).ToList();
+            var totalGoalsFor = 0;
+            var totalGoalsAgainst = 0;
+            var totalWins = 0;
+            var totalLoses = 0;
+            var totalDraws = 0;
+            foreach (var fix in fixtures)
+            {
+                if (fix.TeamHome == team)
+                {
+                    totalGoalsFor += fix.TeamHomeScore;
+                    totalGoalsAgainst += fix.TeamVisitorScore;
+                    if (fix.TeamHomeScore > fix.TeamVisitorScore)
+                    {
+                        totalWins++;
+                    }
+                    else if (fix.TeamHomeScore < fix.TeamVisitorScore)
+                    {
+                        totalLoses++;
+                    }
+                    else
+                    {
+                        totalDraws++;
+                    }
+                }
+                else
+                {
+                    totalGoalsFor += fix.TeamVisitorScore;
+                    totalGoalsAgainst += fix.TeamHomeScore;
+                    if (fix.TeamHomeScore > fix.TeamVisitorScore)
+                    {
+                        totalLoses++;
+                    }
+                    else if (fix.TeamHomeScore < fix.TeamVisitorScore)
+                    {
+                        totalWins++;
+                    }
+                    else
+                    {
+                        totalDraws++;
+                    }
+                }
+            }
+            resultRow.GoalsAgainst = totalGoalsAgainst;
+            resultRow.GoalsFor = totalGoalsFor;
+            resultRow.Wins = totalWins;
+            resultRow.Loses = totalLoses;
+            resultRow.Draws = totalDraws;
+            resultRow.Matches = fixtures.Count();
+        }
+
     }
 }
